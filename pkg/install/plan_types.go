1c467b998c0a3d69fbea7237284223cbe244c96e
Dimitri Koshkin <dimitri.koshkin@gmail.com>
2018-01-08 14:30:07 -0500

    Decouple kubernetes version from KET

--- pkg/install/plan_types.go
+++ pkg/install/plan_types.go
@@ -85,6 +85,10 @@ type Cluster struct {
 	// cluster name, such as kubeconfig files and certificates.
 	// +required
 	Name string
+	// The Kubernetes version to install.
+	// If left blank will be set to the latest tested version.
+	// +default=v1.9.1
+	Version string
 	// The password for the admin user.
 	// If provided, ABAC will be enabled in the cluster.
 	// This field will be removed completely in a future release.
